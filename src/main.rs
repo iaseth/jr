419ed7d5d2b98df068e962618a9daf0b5479e4af
Ankur Seth <iankurseth@gmail.com>
2023-04-18 19:58:34 +0530

    Just listing args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+
+fn main () {
+	let args: Vec<String> = env::args().collect();
+	let filepath = &args[1];
+	println!("Filepath: '{filepath}'");
+
+	for (idx, arg) in args[2..].iter().enumerate() {
+		print!("\tArg #{idx}: '{arg}'\n");
+	}
+
 }
